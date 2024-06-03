c0d37cf9a804818a83d4ee3a4183275b5f545f11
Aleksandr Gladskoy <a.gladskoy@gmail.com>
2024-06-03 15:21:13 +0300

    Задание 6. Обновлён пакет апи тестов

---
+++ src/test/java/ru/gpbitfactory/minibank/middle/createclient/api/AbstractControllerTest.java
@@ -0,0 +1,19 @@
+package ru.gpbitfactory.minibank.middle.createclient.api;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+@SpringBootTest
+public abstract class AbstractControllerTest {
+
+    protected MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp(WebApplicationContext webApplicationContext) {
+        mockMvc = webAppContextSetup(webApplicationContext).build();
+    }
+}
---
+++ src/test/java/ru/gpbitfactory/minibank/middle/createclient/api/ClientsControllerCreateNewClientTest.java
@@ -0,0 +1,98 @@
+package ru.gpbitfactory.minibank.middle.createclient.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import ru.gpbitfactory.minibank.middle.createclient.service.ClientsService;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+class ClientsControllerCreateNewClientTest extends AbstractControllerTest {
+
+    @MockBean
+    private ClientsService clientsService;
+
+    @Test
+    void whenPassToRequestBodyValidTelegramUserId_thenShouldReturnStatus200() throws Exception {
+        var createClientRequest = post("/api/v1/clients")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                        { "telegramUserId": 1 }
+                        """);
+
+        mockMvc.perform(createClientRequest)
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void whenPassToRequestBodyInvalidTelegramUserId_thenShouldReturn400() throws Exception {
+        var createClientRequest = post("/api/v1/clients")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                        { "telegramUserId": "string" }
+                        """);
+
+        mockMvc.perform(createClientRequest)
+                .andDo(print())
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void whenPassToRequestBodyEmptyTelegramUserId_thenShouldReturn422() throws Exception {
+        var createClientRequest = post("/api/v1/clients")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                        { "telegramUserId": " " }
+                        """);
+
+        mockMvc.perform(createClientRequest)
+                .andDo(print())
+                .andExpect(status().isUnprocessableEntity())
+                .andExpect(jsonPath("$.message", is("telegramUserId - не может быть пустым")));
+    }
+
+    @Test
+    void whenNoPassToRequestBodyEmptyTelegramUserId_thenShouldReturn422() throws Exception {
+        var createClientRequest = post("/api/v1/clients")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{}");
+
+        mockMvc.perform(createClientRequest)
+                .andDo(print())
+                .andExpect(status().isUnprocessableEntity())
+                .andExpect(jsonPath("$.message", is("telegramUserId - не может быть пустым")));
+    }
+
+    @Test
+    void whenTelegramUserIdParamIsEqualToZero_thenShouldReturn422() throws Exception {
+        var createClientRequest = post("/api/v1/clients")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                        { "telegramUserId": 0 }
+                        """);
+
+        mockMvc.perform(createClientRequest)
+                .andDo(print())
+                .andExpect(status().isUnprocessableEntity())
+                .andExpect(jsonPath("$.message", is("telegramUserId - может быть только положительным целым числом больше 1")));
+    }
+
+    @Test
+    void whenTelegramUserIdLengthIsGreaterThan16_thenShouldReturn422() throws Exception {
+        var createClientRequest = post("/api/v1/clients")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                        { "telegramUserId": 12345678901234567 }
+                        """);
+
+        mockMvc.perform(createClientRequest)
+                .andDo(print())
+                .andExpect(status().isUnprocessableEntity())
+                .andExpect(jsonPath("$.message", is("telegramUserId - может содержать только число от 1 до 16 символов")));
+    }
+}
