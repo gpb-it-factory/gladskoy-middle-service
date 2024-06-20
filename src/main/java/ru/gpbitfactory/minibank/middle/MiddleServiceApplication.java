f838d0ead433588982a84ea025780ed3e5af471d
Aleksandr Gladskoy <a.gladskoy@gmail.com>
2024-06-20 13:48:06 +0300

    Добавлена конфигурация Open Feign

--- src/main/java/ru/gpbitfactory/minibank/middle/MiddleServiceApplication.java
+++ src/main/java/ru/gpbitfactory/minibank/middle/MiddleServiceApplication.java
@@ -2,7 +2,9 @@ package ru.gpbitfactory.minibank.middle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class MiddleServiceApplication {
 
