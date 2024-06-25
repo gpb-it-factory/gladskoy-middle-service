ae2ba01ce1dbc69d33fe366d2e31994af473d9c0
Aleksandr Gladskoy <a.gladskoy@gmail.com>
2024-06-25 20:48:03 +0300

    Удалена заглушка Backend Service`а (#25)
    * Удалена заглушка Backen Service
    
    * Добавлена конфигурация Open Feign

--- src/main/java/ru/gpbitfactory/minibank/middle/mockbackend/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * <h3>Заглушки для проверки работоспособности сервиса.</h3>
- * TODO Удалить поле реализации реальной интеграции с Backend Service.
- */
-package ru.gpbitfactory.minibank.middle.mockbackend;
